9bdc58e3199948749156467b17e950eb9abaca9a
Paolo PIERRE-ALEXIS <paolopierrealexis@gmail.com>
2025-03-03 16:57:06 +0100

    ok

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ x0, y0 = LARGEUR/2, HAUTEUR/2
 
 root = tk.Tk()
 canva = tk.Canvas(root, width=LARGEUR, height=HAUTEUR, bg="red")
-canva.pack()
+canva.grid(rowspan = 50, columnspan = 50)
 
-class fourmis
+fourmis = tk.Label(canva, )
 
-fourmis = canva.create_rectangle(x0 - 2, y0 - 2, x0 + 2, y0 + 2, fill="black")
+
+
+
+#fourmis = canva.create_rectangle(x0 - 5, y0 - 5, x0 + 5, y0 + 5, fill="red")
 
 
 
