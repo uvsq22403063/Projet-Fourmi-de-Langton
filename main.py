8ae9ef9f6a36b2b3f271a6fdd98eb28efede0ec0
Paolo PIERRE-ALEXIS <paolopierrealexis@gmail.com>
2025-03-03 16:27:06 +0100

    ok

---
+++ main.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+LARGEUR, HAUTEUR = 600, 400
+x0, y0 = LARGEUR/2, HAUTEUR/2
+
+root = tk.Tk()
+canva = tk.Canvas(root, width=LARGEUR, height=HAUTEUR, bg="red")
+canva.pack()
+
+class fourmis
+
+fourmis = canva.create_rectangle(x0 - 2, y0 - 2, x0 + 2, y0 + 2, fill="black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
