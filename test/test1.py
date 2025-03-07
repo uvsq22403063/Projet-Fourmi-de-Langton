97f65ec0a90910c1b715423cf0808c4288a9e826
OdilonBenedetti <obenedetti01@gmail.com>
2025-03-07 15:23:10 +0100

    le même test

--- test/test1.py
+++ test/test1.py
@@ -2,7 +2,7 @@ import tkinter as tk
 racine = tk.Tk()
 racine.title('Test de grille')
 Canvas = tk.Canvas(racine, bg = 'white', height = 800, width = 800)
-L = 50
+L = 10
 couleur = 'white'
 
 
@@ -12,10 +12,10 @@ def ch_couleur():
         couleur = 'black'
     elif couleur == 'black':
         couleur = 'white'
-    carre.config(bg = couleur)
+    carre1.config(fg = couleur)
 
 
-carre = tk.Button(Canvas, height=L, width=L, bg=couleur, command=ch_couleur)
+carre1 = tk.Button(Canvas, height=L, width=L, fg='yellow', command=ch_couleur)
 Canvas.grid()
-carre.grid(row=0, column=0)
+carre1.grid(row = 0, column = 0)
 racine.mainloop()
