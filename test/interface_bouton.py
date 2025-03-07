17bde1df959c94898caa5b3c4070a48de78d736b
Gaston Barthalon <gaston.barthalon@ens.uvsq.fr>
2025-03-07 19:45:29 +0100

    création de l'interface graphique et des boutons

---
+++ test/interface_bouton.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+larg, haut = 600, 600
+
+
+def jouer():
+
+    return
+
+
+def pausse():
+
+    return
+
+
+def nex():
+
+    return
+
+
+window = tk.Tk()
+canva = tk.Canvas(window, bg="white", width=larg, height=haut)
+
+test = tk.Button(window)
+play = tk.Button(window, bg="grey", fg="black", text="PLAY",
+                 font=("Impact", 14), command=jouer)
+pause = tk.Button(window, bg="grey", fg="black", text="PAUSE",
+                  font=("Impact", 14), command=pausse)
+nexte = tk.Button(window, bg="grey", fg="black", text="NEXT",
+                  font=("Impact", 14), command=nex)
+four_plus = tk.Button(window, bg="grey", fg="black", text="+",
+                      font=("Impact", 14), command=nexte)
+four_moins = tk.Button(window, bg="grey", fg="black", text="-",
+                       font=("Impact", 14), command=nexte)
+fourmis = tk.Label(window, text="Fourmis")
+
+play.grid(row=2, column=4)
+nexte.grid(row=2, column=3)
+pause.grid(row=2, column=2)
+four_plus.grid(row=1, column=1)
+four_moins.grid(row=1, column=0)
+test.grid(row=0, column=0, columnspan=2)
+fourmis.grid(row=1, column=0)
+
+canva.grid(row=1, column=2, columnspan=3)
+window.mainloop()
