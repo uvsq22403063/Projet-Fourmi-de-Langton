4b31c036ff215379351fd8e44071ec694ae42721
Gaston Barthalon <gaston.barthalon@ens.uvsq.fr>
2025-03-08 12:45:26 +0100

    a

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ x0, y0 = LARGEUR/2, HAUTEUR/2
 
 root = tk.Tk()
 canva = tk.Canvas(root, width=LARGEUR, height=HAUTEUR, bg="red")
-canva.grid(rowspan = 50, columnspan = 50)
+canva.grid(rowspan=50, columnspan=50)
 
 fourmis = tk.Label(canva, )
 
