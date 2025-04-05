084ef699f9afc855b063b15e72dfe38620aeb78f
Gaston Barthalon <gaston.barthalon@ens.uvsq.fr>
2025-04-05 12:31:46 +0200

    pareil

--- afficher_un_menu.py
+++ afficher_un_menu.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+import subprocess
+
+k, u = 45, 35
+speed = 10
+direction1 = "n"
+nbcolor = 4
 
 window = tk.Tk()
 window.config(bg="lightgrey")
@@ -10,7 +16,13 @@ titre.grid(row=0, pady=50)
 
 
 def prout():
+    global k, u, speed, direction1, nbcolor
+    k, u = int(txt_coordx.get()), int(txt_coordy.get())
+    speed = int(txt_speed.get())
+    direction1 = str(txt_direc.get())
+    nbcolor = int(txt_couleur.get())
 
+    subprocess.run(["python", ".\\test\\couleurs4.py"])
     return
 
 
@@ -29,6 +41,7 @@ frame_nb_couleur.grid(row=5)
 
 
 # ajout des labels et champs de texte pour les variables
+
 # vitesse
 label_speed = tk.Label(frame_speed, text="vitesse (ms):",
                        font=("Arial", 14))
@@ -38,12 +51,15 @@ txt_speed = tk.Entry(frame_speed, font=("Arial", 14))
 txt_speed.pack(side="right")
 
 # coordonées de bases
-label_coord = tk.Label(frame_coord, text="coordonées initiales:",
-                       font=("Arial", 14))
-label_coord.pack(side="left")
+label_coordx = tk.Label(frame_coord, text="x(0 à90):", font=("Arial", 14))
+label_coordx.pack(side="left", ipadx=20)
+txt_coordx = tk.Entry(frame_coord, font=("Arial", 14), width=4)
+txt_coordx.pack(side="left", ipadx=20)
 
-txt_coord = tk.Entry(frame_coord, font=("Arial", 14))
-txt_coord.pack(side="left")
+label_coordy = tk.Label(frame_coord, text="y(0 à 70):", font=("Arial", 14))
+label_coordy.pack(side="left", ipadx=20)
+txt_coordy = tk.Entry(frame_coord, font=("Arial", 14), width=4)
+txt_coordy.pack(side="left", ipadx=20)
 
 # direction
 label_direc = tk.Label(frame_direc, text="direction au départ:",
@@ -62,7 +78,7 @@ txt_couleur = tk.Entry(frame_bg_couleur, font=("Arial", 14))
 txt_couleur.pack(side="left")
 
 # nb de couleurs
-label_nb_couleurs = tk.Label(frame_nb_couleur, text="nombre de couleurs:",
+label_nb_couleurs = tk.Label(frame_nb_couleur, text="nombre couleurs(2 à 6):",
                              font=("Arial", 14))
 label_nb_couleurs.pack(side="left")
 
@@ -70,11 +86,15 @@ txt_couleur = tk.Entry(frame_nb_couleur, font=("Arial", 14))
 txt_couleur.pack(side="left")
 
 
+# bouton jouer
+jouer = tk.Button(window, text="launch", command=prout)
+jouer.grid(row=4, column=1, padx=100)
+
 # création truc en haut à gauche
 menui = tk.Menu(window)
 menu_bar = tk.Menu(menui, tearoff=0)
 
-menu_bar.add_command(label="test", command=prout)
+menu_bar.add_command(label="launch", command=prout)
 menu_bar.add_command(label="quitter", command=window.destroy)
 
 menui.add_cascade(label="fichier", menu=menu_bar)
